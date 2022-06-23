e3fcf9e8d4fc34cee1bc00e211ffef6f2058d8d0
Eduardo San Martin Morote <posva13@gmail.com>
2022-06-23 15:58:43 +0200

    more files for the playground

--- playground/src/routes/[name].vue
+++ playground/src/routes/[name].vue
@@ -1,16 +1,14 @@
 <script lang="ts" setup>
-// import { useRoute } from '~routes/[name]'
-// import { route } from '~custom'
-// import { useRoute } from '@vue-router'
-
 import { useRoute } from '@vue-router'
 
 const route = useRoute()
-if (route.name == '/articles/[id]+') {
+if (route.name == '/articles/[id]') {
   console.log('route.params', route.params.id)
 }
 useRoute('/multiple-[a]-[b]-params').params
-useRoute<'/about'>('/about')
+useRoute<'/[name]'>().params.name
+// @ts-expect-error: /about doesn't have params
+useRoute<'/about'>('/about').params.never}
 </script>
 
 <template>
