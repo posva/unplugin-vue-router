d70de48e6cb29aa7797973bfdf20c5cd2f0bedae
hanlee <me@hanlee.co>
2022-06-29 02:07:49 +0900

    chore: use import from pathe

--- src/core/tree.ts
+++ src/core/tree.ts
@@ -1,4 +1,4 @@
-import { extname } from 'path'
+import { extname } from 'pathe'
 import type { Options } from '../options'
 import { createTreeLeafValue } from './treeLeafValue'
 import type { TreeLeafValue } from './treeLeafValue'
