10c2d9e8f6ee4b4f0f3266054d376cd10651cb5b
Eduardo San Martin Morote <posva13@gmail.com>
2022-09-01 17:30:40 +0200

    wip: volar plugin for syntax on route block

---
+++ volar/routeBlock.ts
@@ -0,0 +1,48 @@
+import type { VueLanguagePlugin } from '@volar/vue-language-core'
+
+const plugin: VueLanguagePlugin = () => {
+  return {
+    getEmbeddedFileNames(fileName, sfc) {
+      const fileNames: string[] = []
+      for (let i = 0; i < sfc.customBlocks.length; i++) {
+        const block = sfc.customBlocks[i]
+        if (block.type === 'route' && block.lang === 'ts') {
+          fileNames.push(`${fileName}.route_${i}.${block.lang}`)
+        }
+      }
+      return fileNames
+    },
+
+    resolveEmbeddedFile(fileName, sfc, embeddedFile) {
+      const match = embeddedFile.fileName.match(/^(.*)\.route_(\d+)\.([^.]+)$/)
+      if (match) {
+        const index = parseInt(match[2])
+        const block = sfc.customBlocks[index]
+        embeddedFile.capabilities = {
+          diagnostics: true,
+          foldingRanges: true,
+          formatting: true,
+          documentSymbol: true,
+          codeActions: true,
+          inlayHints: true,
+        }
+        embeddedFile.isTsHostFile = true
+        embeddedFile.codeGen.addCode2(block.content, 0, {
+          vueTag: 'customBlock',
+          vueTagIndex: index,
+          capabilities: {
+            basic: true,
+            references: true,
+            definitions: true,
+            diagnostic: true,
+            rename: true,
+            completion: true,
+            semanticTokens: true,
+          },
+        })
+      }
+    },
+  }
+}
+
+export default plugin
