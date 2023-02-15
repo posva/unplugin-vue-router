24fb68916484ebdb50327a57c14bd297b1573c49
Eduardo San Martin Morote <posva13@gmail.com>
2023-02-15 18:12:32 +0100

    comments

--- playground/src/pages/partial-[name].vue
+++ playground/src/pages/partial-[name].vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const route = useRoute('/partial-[name]')
+</script>
+
 <template>
   <main>
-    <h1>Partial Param: {{ $route.params.name }}</h1>
+    <h1>Partial Param: {{ route.params.name }}</h1>
   </main>
 </template>
