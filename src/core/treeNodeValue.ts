c89d60236021b7955025e5bf334f5ee9189d939d
Eduardo San Martin Morote <posva13@gmail.com>
2023-04-27 20:52:25 +0200

    refactor: type import

--- src/core/treeNodeValue.ts
+++ src/core/treeNodeValue.ts
@@ -1,4 +1,4 @@
-import { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import { CustomRouteBlock } from './customBlock'
 import { joinPath, mergeRouteRecordOverride } from './utils'
 
