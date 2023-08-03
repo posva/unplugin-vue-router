87d87612e52b4e81bf55eebf84256873f9c8a937
Eduardo San Martin Morote <posva13@gmail.com>
2023-08-03 18:11:55 +0200

    test: dts

--- src/data-fetching_new/defineLoader.spec.ts
+++ src/data-fetching_new/defineLoader.spec.ts
@@ -207,3 +207,66 @@ describe('defineLoader', () => {
     })
   })
 })
+
+// dts testing
+function dts(_fn: () => unknown) {}
+
+dts(async () => {
+  interface UserData {
+    id: string
+    name: string
+  }
+
+  const useDataLoader = defineLoader(async (route) => {
+    const user = {
+      id: route.params.id as string,
+      name: 'Edu',
+    }
+
+    return user
+  })
+
+  expectType<{
+    data: Ref<UserData>
+    error: Ref<unknown>
+    pending: Ref<boolean>
+    refresh: () => Promise<void>
+  }>(useDataLoader())
+
+  // TODO: do we really need to support this usage?
+  const useWithRef = defineLoader(async (route) => {
+    const user = shallowRef<UserData>({
+      id: route.params.id as string,
+      name: 'Edu',
+    })
+
+    return user
+  })
+
+  expectType<{
+    data: Ref<UserData>
+    error: Ref<unknown>
+    pending: Ref<boolean>
+    refresh: () => Promise<void>
+  }>(useWithRef())
+
+  async function loaderUser() {
+    const user: UserData = {
+      id: 'one',
+      name: 'Edu',
+    }
+
+    return user
+  }
+
+  expectType<{ data: Ref<UserData | undefined> }>(
+    defineLoader(loaderUser, { lazy: true })()
+  )
+  expectType<{ data: Ref<UserData> }>(defineLoader(loaderUser, {})())
+  expectType<{ data: Ref<UserData> }>(
+    defineLoader(loaderUser, { lazy: false })()
+  )
+  expectType<{ data: Ref<UserData> }>(
+    defineLoader(loaderUser, { lazy: false })()
+  )
+})
