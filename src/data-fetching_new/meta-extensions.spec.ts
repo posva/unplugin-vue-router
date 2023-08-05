a6efe8707092dd89b7c024e8546f07380db489b8
Eduardo San Martin Morote <posva13@gmail.com>
2023-08-05 17:03:55 +0200

    test: dts meta extensions

---
+++ src/data-fetching_new/meta-extensions.spec.ts
@@ -0,0 +1,63 @@
+import { describe, it } from 'vitest'
+import { defineComponent } from 'vue'
+import { createRouter, createMemoryHistory } from 'vue-router'
+import { defineLoader } from './defineLoader'
+
+describe('meta-extensions', () => {
+  it('has tds', () => {})
+})
+
+async function _dts() {
+  const component = defineComponent({})
+  createRouter({
+    history: createMemoryHistory(),
+    routes: [
+      // empty
+      {
+        path: '/',
+        component,
+        meta: {
+          loaders: [],
+        },
+      },
+
+      // mixed
+      {
+        path: '/',
+        component,
+        meta: {
+          loaders: [
+            defineLoader(async () => ({ name: 'foo' })),
+            defineLoader(async () => ({ name: 'foo' }), {}),
+            defineLoader(async () => ({ name: 'foo' }), { lazy: true }),
+            defineLoader(async () => ({ name: 'foo' }), { lazy: false }),
+          ],
+        },
+      },
+
+      // only lazy: true
+      {
+        path: '/',
+        component,
+        meta: {
+          loaders: [
+            defineLoader(async () => ({ name: 'foo' }), { lazy: true }),
+            defineLoader(async () => ({ name: 'foo' }), { lazy: true }),
+          ],
+        },
+      },
+
+      // only lazy: false
+      {
+        path: '/',
+        component,
+        meta: {
+          loaders: [
+            defineLoader(async () => ({ name: 'foo' }), { lazy: false }),
+            defineLoader(async () => ({ name: 'foo' }), { lazy: false }),
+          ],
+        },
+      },
+    ],
+  })
+}
