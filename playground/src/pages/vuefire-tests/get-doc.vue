75a55f271deffef794662c45ae4ea91ca2f9f116
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-22 11:20:40 +0200

    new pages and stuff

---
+++ playground/src/pages/vuefire-tests/get-doc.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const useData = defineLoader('/vuefire-tests/get-doc', async (to) => {
+  return
+})
+</script>
+
+<template>
+  <div>Example to check syntax with loaders</div>
+</template>
