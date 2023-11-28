9bb3da891a02c26d0aa5a1df9e97f656d6780306
Eduardo San Martin Morote <posva13@gmail.com>
2023-11-28 16:19:04 +0100

    refactor: playground tests

--- playground/src/pages/users/[id].vue
+++ playground/src/pages/users/[id].vue
@@ -1,9 +1,32 @@
 <script lang="ts">
 export const myExport = 'OUTSIDE SETUP TEST'
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+// NOTE: it's a bit different from the one in /[name].vue
+export const useUserData = defineBasicLoader(
+  '/users/[id]',
+  async (route) => {
+    await delay(1000)
+    const user = {
+      id: route.params.id || 24,
+      // @ts-expect-error: no id param!
+      name: route.params.name || 'Edu',
+      when: new Date().toUTCString(),
+    }
+    return user
+  },
+  {
+    key: 'user-id',
+    lazy: false,
+  }
+)
 </script>
 
 <script lang="ts" setup>
 const route = useRoute('/users/[id]')
+
+const { data: user, pending, error } = useUserData()
 
 definePage({
   beforeEnter(to) {
@@ -21,6 +44,18 @@ const MY_VAL = 'INSIDE SETUP TEST'
   <main>
     <pre>User: {{ route.params.id }}</pre>
     <p>{{ MY_VAL }}</p>
+
+    <RouterLink :to="{ params: { id: Number(route.params.id) - 1 } }"
+      >Previous</RouterLink
+    >
+    |
+    <RouterLink :to="{ params: { id: Number(route.params.id) + 1 } }"
+      >Next</RouterLink
+    >
+
+    <pre v-if="pending">Loading...</pre>
+    <pre v-else-if="error">Error: {{ error }}</pre>
+    <pre v-else>{{ user }}</pre>
   </main>
 </template>
 
