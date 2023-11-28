b3ce5ad04157a019109a9ecfdcde485310f7e5c0
Eduardo San Martin Morote <posva13@gmail.com>
2023-11-28 17:34:20 +0100

    test: fix compat node 16

--- tests/data-loaders/RouterViewMock.vue
+++ tests/data-loaders/RouterViewMock.vue
@@ -5,8 +5,11 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 
 const viewComponent = computed(() => {
-  return route.matched.findLast((record) => record.components?.default)
-    ?.components?.default
+  // NOTE: .findLast is node >=18
+  return route.matched
+    .slice()
+    .reverse()
+    .find((record) => record.components?.default)?.components?.default
 })
 </script>
 
