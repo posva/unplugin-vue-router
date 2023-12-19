23f9bb85262b0ca2963c2a4e76a4a4ee636f47d5
Eduardo San Martin Morote <posva13@gmail.com>
2023-12-19 10:55:28 +0100

    chore: nuxt is also broken

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -10,4 +10,8 @@ export default defineNuxtConfig({
     pageTransition: false,
     layoutTransition: false,
   },
+
+  experimental: {
+    typedPages: true,
+  },
 })
--- examples/nuxt/pages/users/[id].vue
+++ examples/nuxt/pages/users/[id].vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+// import { _defineBasicLoader as defineBasicLoader } from 'unplugin-vue-router/runtime'
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 export const useUserData = defineBasicLoader(
