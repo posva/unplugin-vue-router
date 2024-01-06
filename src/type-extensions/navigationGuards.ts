3f01155e8cb609d1af07b115103907176d44aab8
Eduardo San Martin Morote <posva13@gmail.com>
2024-01-06 10:08:06 +0100

    fix(types): correct types in navigation guards

--- src/type-extensions/navigationGuards.ts
+++ src/type-extensions/navigationGuards.ts
@@ -21,9 +21,25 @@ type NavigationGuardReturn =
 export interface NavigationGuardWithThis<T> {
   (
     this: T,
+    to: _RouteLocationNormalized,
+    from: _RouteLocationNormalizedLoaded,
+    // intentionally not typed to make people use the return
+    next: NavigationGuardNext
+  ): NavigationGuardReturn | Promise<NavigationGuardReturn>
+}
+
+/**
+ * In `router.beforeResolve((to) => {})`, the `to` is typed as `RouteLocationNormalizedLoaded`, not
+ * `RouteLocationNormalized` like in `router.beforeEach()`. In practice it doesn't change much as users do not rely on
+ * the difference between them but if we update the type in vue-router, we will have to update this type too.
+ * @internal
+ */
+export interface _NavigationGuardResolved {
+  (
+    this: undefined,
     to: _RouteLocationNormalizedLoaded,
     from: _RouteLocationNormalizedLoaded,
-    // intentionally not typed to make people use the other version
+    // intentionally not typed to make people use the return
     next: NavigationGuardNext
   ): NavigationGuardReturn | Promise<NavigationGuardReturn>
 }
@@ -31,7 +47,7 @@ export interface NavigationGuardWithThis<T> {
 export interface NavigationGuard {
   (
     to: _RouteLocationNormalized,
-    from: _RouteLocationNormalized,
+    from: _RouteLocationNormalizedLoaded,
     // intentionally not typed to make people use the other version
     next: NavigationGuardNext
   ): NavigationGuardReturn | Promise<NavigationGuardReturn>
