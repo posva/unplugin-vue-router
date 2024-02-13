e691a120eb9d6aa6d0eb469860e412e615ca21f4
Eduardo San Martin Morote <posva13@gmail.com>
2024-02-13 15:36:40 +0100

    chore: up auto imports [skip ci]

--- playground/auto-imports.d.ts
+++ playground/auto-imports.d.ts
@@ -7,7 +7,7 @@ export {}
 declare global {
   const defineBasicLoader: typeof import('vue-router/auto')['defineBasicLoader']
   const defineLoader: typeof import('unplugin-vue-router/runtime')['_defineLoader']
-  const definePage: typeof import('unplugin-vue-router/runtime')['_definePage']
+  const definePage: typeof import('unplugin-vue-router/runtime')['definePage']
   const onBeforeRouteLeave: typeof import('vue-router/auto')['onBeforeRouteLeave']
   const onBeforeRouteUpdate: typeof import('vue-router/auto')['onBeforeRouteUpdate']
   const useRoute: typeof import('vue-router/auto')['useRoute']
