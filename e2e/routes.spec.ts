9d21d2db33a1e5812a1fe65cb9d2a5ea3c34214d
Eduardo San Martin Morote <posva13@gmail.com>
2024-05-27 15:16:54 +0200

    test: group e2e tests

--- e2e/routes.spec.ts
+++ e2e/routes.spec.ts
@@ -1,7 +1,7 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { createRoutesContext } from '../src/core/context'
-import { DEFAULT_OPTIONS, resolveOptions } from '../src/options'
-import { fileURLToPath, URL } from 'url'
+import { resolveOptions } from '../src/options'
+import { fileURLToPath, URL } from 'node:url'
 import { normalize, join } from 'pathe'
 
 const __dirname = fileURLToPath(new URL('./', import.meta.url))
@@ -10,24 +10,62 @@ const __dirname = fileURLToPath(new URL('./', import.meta.url))
  * This is a simple full test to check that all filenames are valid in different environment (windows, mac, linux).
  */
 
-it('generates the routes', async () => {
-  const context = createRoutesContext(
-    resolveOptions({
-      // dts: join(__dirname, './__types.d.ts'),
-      dts: false,
-      logs: false,
-      routesFolder: [{ src: join(__dirname, './fixtures/filenames/routes') }],
-    })
-  )
+describe('e2e routes', () => {
+  it('generates the routes', async () => {
+    const context = createRoutesContext(
+      resolveOptions({
+        // dts: join(__dirname, './__types.d.ts'),
+        dts: false,
+        logs: false,
+        watch: false,
+        routesFolder: [{ src: join(__dirname, './fixtures/filenames/routes') }],
+      })
+    )
 
-  await context.scanPages()
-  expect(
-    context
-      .generateRoutes()
-      .replace(
-        /import\(["'](.+?)["']\)/g,
-        (_, filePath) => `import('${normalize(filePath)}')`
-      )
-      .replace(/(import\(["'])(?:.+?)fixtures\/filenames/gi, '$1')
-  ).toMatchSnapshot()
+    await context.scanPages()
+    expect(
+      context
+        .generateRoutes()
+        .replace(
+          /import\(["'](.+?)["']\)/g,
+          (_, filePath) => `import('${normalize(filePath)}')`
+        )
+        .replace(/(import\(["'])(?:.+?)fixtures\/filenames/gi, '$1')
+    ).toMatchSnapshot()
+  })
+
+  it.skip('works with mixed extensions', async () => {
+    const context = createRoutesContext(
+      resolveOptions({
+        dts: false,
+        logs: false,
+        watch: false,
+        routesFolder: [
+          {
+            src: join(__dirname, './fixtures/filenames/multi-extensions'),
+            exclude: join(
+              __dirname,
+              './fixtures/filenames/multi-extensions/docs'
+            ),
+          },
+          {
+            src: join(__dirname, './fixtures/filenames/multi-extensions/docs'),
+            extensions: ['.md', '.vue'],
+            path: 'docs/[lang]/',
+          },
+        ],
+      })
+    )
+
+    await context.scanPages()
+    expect(
+      context
+        .generateRoutes()
+        .replace(
+          /import\(["'](.+?)["']\)/g,
+          (_, filePath) => `import('${normalize(filePath)}')`
+        )
+        .replace(/(import\(["'])(?:.+?)fixtures\/filenames/gi, '$1')
+    ).toMatchSnapshot()
+  })
 })
