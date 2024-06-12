7d9a6bb10fb0aee8644277a0f700f1992d059e1d
Eduardo San Martin Morote <posva13@gmail.com>
2024-06-12 17:09:42 +0200

    refactor: fix module augmentation

--- docs/.vitepress/twoslash-files.ts
+++ docs/.vitepress/twoslash-files.ts
@@ -1,13 +1,13 @@
 export const typedRouterFile = `
-import type {
-  RouteRecordInfo,
-  ParamValue,
-  ParamValueOneOrMore,
-  ParamValueZeroOrMore,
-  ParamValueZeroOrOne,
-} from 'vue-router'
-
 declare module 'vue-router/auto-routes' {
+  import type {
+    RouteRecordInfo,
+    ParamValue,
+    ParamValueOneOrMore,
+    ParamValueZeroOrMore,
+    ParamValueZeroOrOne,
+  } from 'vue-router'
+
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>
     '/users': RouteRecordInfo<
