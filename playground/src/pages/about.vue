0af47d21b43d63294dc1a22603d71e1626d3b5bc
Eduardo San Martin Morote <posva13@gmail.com>
2025-01-26 13:54:10 +0100

    chore: playground tests hmr

--- playground/src/pages/about.vue
+++ playground/src/pages/about.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { routes } from 'vue-router/auto-routes'
+
+definePage({
+  meta: {
+    n: 12,
+  },
+})
+
+onMounted(() => {
+  console.log('routes', routes)
+})
+</script>
+
 <template>
   <main>
     <h1>About</h1>
+
+    <p>Increment to test HMR: 1</p>
+
+    <pre>{{ $route.meta }}</pre>
   </main>
 </template>
