8c5ad57c2dd746fac3dea425879887de5f95220c
Eduardo San Martin Morote <posva13@gmail.com>
2025-01-26 13:51:42 +0100

    chore: playground group

---
+++ playground/src/pages/(test-group).vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePage({
+  meta: {
+    name: '(test group root)',
+  },
+})
+</script>
+
+<template>
+  <RouterView />
+
+  <hr />
+
+  <pre>{{ $route.meta }}</pre>
+</template>
--- playground/src/pages/(test-group)/test-group-child.vue
+++ playground/src/pages/(test-group)/test-group-child.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+definePage({
+  meta: {
+    name: 'overriden by child in group',
+    other: 'foo',
+  },
+})
+</script>
+
 <template>
-  <h1>Test group child (resolves to root)</h1>
+  <h2>Test group child (resolves to root)</h2>
 </template>
