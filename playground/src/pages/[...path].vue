c34c811597f92f553e0c3a12fa2d16e0dc35bff4
Anoesj <anoesjsadraee@gmail.com>
2025-03-19 18:10:15 +0100

    chore: playground remove redundant checks

--- playground/src/pages/[...path].vue
+++ playground/src/pages/[...path].vue
@@ -2,7 +2,7 @@
   <main>
     <h1>Not Found</h1>
     <p>
-      {{ $route.name === '/[...path]' && $route.params.path }} does not exist.
+      {{ $route.params.path }} does not exist.
     </p>
   </main>
 </template>
