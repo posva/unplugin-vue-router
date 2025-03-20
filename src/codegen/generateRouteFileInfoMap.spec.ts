0829c2f2b6b386a55f9c41bfbe6e5a38a9114818
Anoesj <anoesjsadraee@gmail.com>
2025-03-20 14:28:34 +0100

    test: add tests for generating `RouteFileInfoMap`

---
+++ src/codegen/generateRouteFileInfoMap.spec.ts
@@ -0,0 +1,87 @@
+import { describe, expect, it } from 'vitest'
+import { generateRouteFileInfoMap } from './generateRouteFileInfoMap'
+import { PrefixTree } from '../core/tree'
+import { resolveOptions } from '../options'
+
+const DEFAULT_OPTIONS = resolveOptions({})
+
+function formatExports(exports: string) {
+  return exports
+    .split('\n')
+    .filter((line) => line.length > 0)
+    .join('\n')
+}
+
+describe('generateRouteFileInfoMap', () => {
+  it('works with some paths at root', () => {
+    const tree = new PrefixTree(DEFAULT_OPTIONS)
+    tree.insert('index', 'src/pages/index.vue')
+    tree.insert('a', 'src/pages/a.vue')
+    tree.insert('b', 'src/pages/b.vue')
+    tree.insert('c', 'src/pages/c.vue')
+    expect(formatExports(generateRouteFileInfoMap(tree, { root: '' })))
+      .toMatchInlineSnapshot(`
+      "export interface RouteFileInfoMap {
+        'src/pages/index.vue': {
+          routes: '/',
+          views: never,
+        },
+        'src/pages/a.vue': {
+          routes: '/a',
+          views: never,
+        },
+        'src/pages/b.vue': {
+          routes: '/b',
+          views: never,
+        },
+        'src/pages/c.vue': {
+          routes: '/c',
+          views: never,
+        },
+      }"
+    `)
+  })
+
+  it('works with children', () => {
+    const tree = new PrefixTree(DEFAULT_OPTIONS)
+    tree.insert('parent', 'src/pages/parent.vue')
+    tree.insert('parent/child', 'src/pages/parent/child.vue')
+    expect(formatExports(generateRouteFileInfoMap(tree, { root: '' })))
+      .toMatchInlineSnapshot(`
+      "export interface RouteFileInfoMap {
+        'src/pages/parent.vue': {
+          routes: '/parent' | '/parent/child',
+          views: 'default',
+        },
+        'src/pages/parent/child.vue': {
+          routes: '/parent/child',
+          views: never,
+        },
+      }"
+    `)
+  })
+
+  it('works with named views', () => {
+    const tree = new PrefixTree(DEFAULT_OPTIONS)
+    tree.insert('parent', 'src/pages/parent.vue')
+    tree.insert('parent/child', 'src/pages/parent/child.vue')
+    tree.insert('parent/child@test', 'src/pages/parent/child@test.vue')
+    expect(formatExports(generateRouteFileInfoMap(tree, { root: '' })))
+      .toMatchInlineSnapshot(`
+      "export interface RouteFileInfoMap {
+        'src/pages/parent.vue': {
+          routes: '/parent' | '/parent/child',
+          views: 'default' | 'test',
+        },
+        'src/pages/parent/child.vue': {
+          routes: '/parent/child',
+          views: never,
+        },
+        'src/pages/parent/child@test.vue': {
+          routes: '/parent/child',
+          views: never,
+        },
+      }"
+    `)
+  })
+})
