4ccf2e4e503748dd2ec139dd04a7fb0ce648c237
Anoesj <anoesjsadraee@gmail.com>
2025-03-20 12:09:51 +0100

    chore: refactor volar plugin sfc-route-blocks and add todo

--- src/volar/entries/sfc-route-blocks.ts
+++ src/volar/entries/sfc-route-blocks.ts
@@ -1,30 +1,38 @@
-import type { VueLanguagePlugin } from '@vue/language-core'
+import { allCodeFeatures, type VueLanguagePlugin } from '@vue/language-core'
 
 const plugin: VueLanguagePlugin = () => {
+  const routeBlockIdPrefix = 'route_'
+  const routeBlockIdRe = new RegExp(`^${routeBlockIdPrefix}(\\d+)$`)
+
   return {
     version: 2.1,
     getEmbeddedCodes(_fileName, sfc) {
-      const names = []
+      const embeddedCodes = []
 
+      // we add an embedded code for every route block we find with the same index as the block
       for (let i = 0; i < sfc.customBlocks.length; i++) {
         const block = sfc.customBlocks[i]!
 
-        // TODO: `<route>` block without `lang` is still interpreted as text right now, it seems
+        // TODO:
+        // `<route>` blocks without `lang="json"` are still interpreted as text right now.
+        // See: https://github.com/vuejs/language-tools/issues/185#issuecomment-1173742726
+        // This seems to be because `custom_block_x` is still seen as txt, even though the corresponding `route_x` is json.
         if (block.type === 'route') {
           const lang = block.lang === 'txt' ? 'json' : block.lang
-          names.push({ id: `route_${i}`, lang: lang || 'json' })
+          embeddedCodes.push({ id: `${routeBlockIdPrefix}${i}`, lang })
         }
       }
 
-      return names
+      return embeddedCodes
     },
     resolveEmbeddedCode(_fileName, sfc, embeddedCode) {
-      const match = embeddedCode.id.match(/^route_(\d+)$/)
+      const match = embeddedCode.id.match(routeBlockIdRe)
 
-      if (match && match[1] !== undefined) {
-        const index = parseInt(match[1])
-        const block = sfc.customBlocks[index]
+      if (match) {
+        const i = parseInt(match[1]!)
+        const block = sfc.customBlocks[i]
 
+        // this shouldn't happen, but just in case
         if (!block) {
           return
         }
@@ -33,14 +41,7 @@ const plugin: VueLanguagePlugin = () => {
           block.content,
           block.name,
           0,
-          {
-            verification: true,
-            completion: true,
-            semantic: true,
-            navigation: true,
-            structure: true,
-            format: true,
-          },
+          allCodeFeatures,
         ])
       }
     },
