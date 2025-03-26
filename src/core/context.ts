39f3cb4b2e0a583df4f28a92e93a35d6e72ede21
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-03-26 09:31:16 -0700

    chore: switch to tinyglobby

--- src/core/context.ts
+++ src/core/context.ts
@@ -5,7 +5,7 @@ import { asRoutePath, ImportsMap, logTree, throttle } from './utils'
 import { generateRouteNamedMap } from '../codegen/generateRouteMap'
 import { MODULE_ROUTES_PATH, MODULE_VUE_ROUTER_AUTO } from './moduleConstants'
 import { generateRouteRecord } from '../codegen/generateRouteRecords'
-import fg from 'fast-glob'
+import { glob } from 'tinyglobby'
 import { dirname, relative, resolve } from 'pathe'
 import { ServerContext } from '../options'
 import { getRouteBlock } from './customBlock'
@@ -73,11 +73,12 @@ export function createRoutesContext(options: ResolvedOptions) {
             f.startsWith('**') ? f : relative(folder.src, f)
           )
 
-          return fg(folder.pattern, {
+          return glob(folder.pattern, {
             cwd: folder.src,
             // TODO: do they return the symbolic link path or the original file?
             // followSymbolicLinks: false,
             ignore: ignorePattern,
+            expandDirectories: false,
           }).then((files) =>
             Promise.all(
               files
