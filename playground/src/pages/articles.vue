65f44fd0d7a12dfa85cbfa8706674ebba2eb090b
Eduardo San Martin Morote <posva13@gmail.com>
2025-06-10 11:33:33 +0200

    chore: testing code

--- playground/src/pages/articles.vue
+++ playground/src/pages/articles.vue
@@ -1,4 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute()
+
+if ('id' in route.params) {
+  route.params.id satisfies string | [string, ...string[]]
+}
+</script>
 
 <template>
   <h1>Articles</h1>
