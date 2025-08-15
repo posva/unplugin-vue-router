c57cf4ee5e533dfbccce72952c1614c80f2b7088
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-15 17:39:22 +0200

    chore: add meta to page

--- playground-experimental/src/pages/[...404].vue
+++ playground-experimental/src/pages/[...404].vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+definePage({
+  meta: {
+    title: 'Not Found',
+    description: 'This page does not exist.',
+  },
+})
+</script>
+
 <template>
   <main>
     <h1>Not Found</h1>
