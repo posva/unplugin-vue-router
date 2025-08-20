a9d2ea0793ec8a014ff0d357d31630879f29fc0f
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-20 13:55:53 +0200

    chore: rename

--- playground-experimental/src/pages/[...path].vue
+++ playground-experimental/src/pages/[...path].vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+import { definePage } from 'unplugin-vue-router/experimental-param-parsers'
+import doc from './[...404].vue?raw'
+
+const route = useRoute()
+route.params.path
+
 definePage({
   meta: {
     title: 'Not Found',
     description: 'This page does not exist.',
+    docLength: doc.length,
+  },
+  params: {
+    query: {
+      page: {
+        parser: 'int',
+        default: 1,
+        format: 'value',
+      },
+    },
   },
 })
 </script>
