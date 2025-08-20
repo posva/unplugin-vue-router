068ae4d79d43b0aa9878f93156e46a387a01e754
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-20 14:04:59 +0200

    refactor: improve type for param parsers

--- playground-experimental/src/pages/users/[userId=int].vue
+++ playground-experimental/src/pages/users/[userId=int].vue
@@ -1,15 +1,22 @@
 <script lang="ts" setup>
 const route = useRoute()
 route.params.userId
+route.params.anyParam
+route.params.page
 
 definePage({
-  path: '/users/:userId',
-  // query: {
-  //   page: {
-  //     type: 'int',
-  //     optional: true,
-  //   },
-  // },
+  // this doesn't work in custom param version and should warn
+  // path: '/users/:userId',
+  params: {
+    query: {
+      anyParam: {},
+      page: {
+        parser: 'int',
+        default: 1,
+        format: 'value',
+      },
+    },
+  },
   meta: {
     // te: 3, 2
   },
@@ -18,4 +25,6 @@ definePage({
 
 <template>
   <h1>{{ String($route.name) }} - {{ $route.path }}</h1>
+
+  <pre>{{ route.params }}</pre>
 </template>
