c9200450c6fe9558b8de23ba27b3eb752cceaef8
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-23 14:48:31 +0200

    feat: remove 'both' format for query params, default to 'value'

--- src/core/definePage.ts
+++ src/core/definePage.ts
@@ -314,10 +314,7 @@ function extractQueryParams(
               paramProp.key.name === 'format' &&
               paramProp.value.type === 'StringLiteral'
             ) {
-              paramInfo.format = paramProp.value.value as
-                | 'value'
-                | 'array'
-                | 'both'
+              paramInfo.format = paramProp.value.value as 'value' | 'array'
             } else if (paramProp.key.name === 'default') {
               if (
                 typeof paramProp.value.extra?.raw === 'string' &&
