7803ffbe4ee2bdc866ef77135d07fb8b47d11c16
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2026-01-04 00:22:02 +0100

    Feat: infer narrow types for routesFolder

--- src/vite.ts
+++ src/vite.ts
@@ -1,3 +1,11 @@
 import unplugin from '.'
+import type { Options } from './options'
+import type { Plugin as VitePlugin } from 'vite'
 
-export default unplugin.vite
+export default unplugin.vite as <
+  TFilePatterns extends readonly string[] = string[],
+  TExclude extends readonly string[] = string[],
+  TExtensions extends readonly string[] = string[],
+>(
+  options?: Options<[...TFilePatterns], [...TExclude], [...TExtensions]>
+) => VitePlugin
